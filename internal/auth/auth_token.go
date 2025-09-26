4e7334026d1c7e6682707c826a6a8a4a5fde7e9a
Olivier Meunier <olivier@neokraft.net>
2025-09-26 14:27:00 +0200

    Update a token.last_used in the background.
    This value doesn't not need to be processed during the authentication
    sequence and can happen anytime once a token has been validated.
    It gives a tiny performance boost for every API request using a token.

--- internal/auth/auth_token.go
+++ internal/auth/auth_token.go
@@ -6,6 +6,7 @@ package auth
 
 import (
 	"errors"
+	"log/slog"
 	"net/http"
 	"strings"
 	"time"
@@ -48,16 +49,19 @@ func (p *TokenAuthProvider) Authenticate(w http.ResponseWriter, r *http.Request)
 		return r, err
 	}
 
-	if err := res.Token.Update(goqu.Record{
-		"last_used": time.Now().UTC(),
-	}); err != nil {
-		return r, err
-	}
-
 	if res.Token.IsExpired() {
 		p.denyAccess(w)
 		return r, errors.New("expired token")
 	}
+
+	lastUsed := time.Now().UTC()
+	go func() {
+		if err := res.Token.Update(goqu.Record{
+			"last_used": lastUsed,
+		}); err != nil {
+			slog.Error("token update", slog.Any("err", err))
+		}
+	}()
 
 	return SetRequestAuthInfo(r, &Info{
 		Provider: &ProviderInfo{
