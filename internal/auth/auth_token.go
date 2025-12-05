95843bfbe37d2e657f0a35839502a56a716db867
Olivier Meunier <olivier@neokraft.net>
2025-12-05 16:20:53 +0100

    Refactored internal/auth
    Instead of authentication providers having a method to indicate when
    they're active or not, we compose a chain of HTTP handlers where
    providers need to add themselves based on their own conditions.
    
    This lets us do a few interesting things like:
    - stop when a provider meets some conditions (ie. an HTTP header)
    - prepare information for the next provider to pick up (for a forwarded auth)
    - return an HTTP response and terminate everything if we need to.
    
    The public internal/auth API remains unchanged.

--- internal/auth/auth_token.go
+++ internal/auth/auth_token.go
@@ -15,24 +15,36 @@ import (
 	"codeberg.org/readeck/readeck/configs"
 	"codeberg.org/readeck/readeck/internal/acls"
 	"codeberg.org/readeck/readeck/internal/auth/tokens"
+	"codeberg.org/readeck/readeck/pkg/ctxr"
 )
+
+type ctxTokenKey struct{}
+
+var withToken, getToken = ctxr.WithGetter[string](ctxTokenKey{})
 
 // TokenAuthProvider handles authentication using a bearer token
 // passed in the request "Authorization" header with the scheme
 // "Bearer".
 type TokenAuthProvider struct{}
 
-// IsActive returns true when the client submits a bearer token.
-func (p *TokenAuthProvider) IsActive(r *http.Request) bool {
-	_, ok := p.getToken(r)
-	return ok
+// Handler sets [TokenAuthProvider] when the client submit an Authorization header
+// that contains a token.
+func (p *TokenAuthProvider) Handler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		if token, ok := p.getToken(r); ok {
+			ctx = withProvider(ctx, p)
+			ctx = withToken(ctx, token)
+		}
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
 // Authenticate performs the authentication using the "Authorization: Bearer"
 // header provided.
 func (p *TokenAuthProvider) Authenticate(w http.ResponseWriter, r *http.Request) (*http.Request, error) {
-	token, ok := p.getToken(r)
-	if !ok {
+	token := getToken(r.Context())
+	if token == "" {
 		p.denyAccess(w)
 		return r, errors.New("invalid authentication header")
 	}
@@ -60,7 +72,7 @@ func (p *TokenAuthProvider) Authenticate(w http.ResponseWriter, r *http.Request)
 		return r, errors.New("expired token")
 	}
 
-	return SetRequestAuthInfo(r, &Info{
+	ctx := withAuthInfo(r.Context(), &Info{
 		Provider: &ProviderInfo{
 			Name:        "bearer token",
 			Application: res.Token.Application,
@@ -68,7 +80,8 @@ func (p *TokenAuthProvider) Authenticate(w http.ResponseWriter, r *http.Request)
 			ID:          res.Token.UID,
 		},
 		User: res.User,
-	}), nil
+	})
+	return r.WithContext(ctx), nil
 }
 
 // HasPermission checks the permission on the current authentication provider role
@@ -103,26 +116,23 @@ func (p *TokenAuthProvider) CsrfExempt(_ *http.Request) bool {
 }
 
 // getToken reads the token from the "Authorization" header.
-func (p *TokenAuthProvider) getToken(r *http.Request) (token string, ok bool) {
+func (p *TokenAuthProvider) getToken(r *http.Request) (string, bool) {
 	auth := r.Header.Get("Authorization")
 	if auth == "" {
-		return
+		return "", false
 	}
 
 	// Try basic auth first
-	if _, token, ok = r.BasicAuth(); ok {
-		return
+	if _, token, ok := r.BasicAuth(); ok {
+		return token, ok
 	}
 
 	// Bearer token otherwise
-	token = ""
 	const prefix = "Bearer "
 	if len(auth) < len(prefix) || !strings.EqualFold(auth[:len(prefix)], prefix) {
-		return
+		return "", false
 	}
-	token = auth[len(prefix):]
-	ok = true
-	return
+	return auth[len(prefix):], true
 }
 
 func (p *TokenAuthProvider) denyAccess(w http.ResponseWriter) {
