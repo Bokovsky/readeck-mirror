c0a102ebfabd507a7ad70979a3efe39ad3affde7
Olivier Meunier <olivier@neokraft.net>
2026-04-02 22:07:39 +0200

    Added dry-run and json options to the user command
    Whit the flag "-dry-run" (or "-n") the user command doesn't perform any
    action and report whether a user will be created or updated.
    
    The "-json" flag returns the result as a JSON object with the following
    content:
    
    {"exists":false,"status":"create","message":"User \"alice\" successfully created"}
    
    or
    
    {"exists":true,"status":"update","message":"User \"alice\" successfully updated"}
    
    Those two flags combined can help when checking whether a user already
    exists or not.
    
    Resolves #1172

--- internal/app/user.go
+++ internal/app/user.go
@@ -6,6 +6,7 @@ package app
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
@@ -33,17 +34,29 @@ func init() {
 	})
 }
 
+type userResult struct {
+	Exists  bool   `json:"exists"`
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
 type userFlags struct {
 	appFlags
 	User       string
 	Password   string
 	Email      string
 	Group      string
+	DryRun     bool
 	RemoveTOTP bool
+	JSON       bool
 }
 
 func (f *userFlags) Flags() *flag.FlagSet {
 	fs := f.appFlags.Flags()
+	fs.BoolVar(&f.DryRun, "dry-run", false, "do not perform actions")
+	fs.BoolVar(&f.DryRun, "n", false, "dry-run (shorthand)")
+	fs.BoolVar(&f.JSON, "json", false, "output result in JSON")
+
 	fs.StringVar(&f.Email, "email", "", "email address")
 	fs.StringVar(&f.Group, "group", "", "group")
 	fs.StringVar(&f.Password, "password", "", strings.TrimSpace(`
@@ -201,6 +214,14 @@ func (f *userFlags) passwordFromEnv(varName string) (string, error) {
 	return password, nil
 }
 
+func (f *userFlags) output(res *userResult) {
+	if f.JSON {
+		json.NewEncoder(os.Stdout).Encode(res) //nolint:errcheck
+		return
+	}
+	fmt.Printf("⭐ %s\n", res.Message)
+}
+
 func runUser(_ context.Context, args []string) (err error) {
 	var flags userFlags
 	if err = flags.Flags().Parse(args); err != nil {
@@ -239,6 +260,22 @@ func runUser(_ context.Context, args []string) (err error) {
 		}
 	}
 
+	res := &userResult{}
+
+	if user.ID == 0 {
+		res.Status = "create"
+		res.Message = fmt.Sprintf(`User "%s" successfully created`, user.Username)
+	} else {
+		res.Exists = true
+		res.Status = "update"
+		res.Message = fmt.Sprintf(`User "%s" successfully updated`, user.Username)
+	}
+
+	if flags.DryRun {
+		flags.output(res)
+		return nil
+	}
+
 	if err = flags.setPassword(user); err != nil {
 		return err
 	}
@@ -247,16 +284,14 @@ func runUser(_ context.Context, args []string) (err error) {
 	flags.setEmail(user)
 	flags.removeTOTP(user)
 
-	msg := "created"
 	if user.ID == 0 {
 		_, err = db.Q().Insert(db.TableUser).Rows(user).Prepared(true).Executor().Exec()
 	} else {
 		err = user.Save()
-		msg = "updated"
 	}
 
 	if err == nil {
-		fmt.Printf("⭐ User \"%s\" successfully %s.\n", user.Username, msg)
+		flags.output(res)
 	}
 
 	return err
